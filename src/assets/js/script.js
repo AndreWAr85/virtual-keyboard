55ba07a8f43841f1a42d2b47f1a5e257f2e39a46
AndreWAr85 <andreyarabchyk@gmail.com>
2023-04-29 15:37:40 +0200

    feat: add keysValue.js

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,9 +1,11 @@
+import { keysValue } from './keysValue.js';
+console.log(keysValue)
 const main = document.querySelector('main');
 
-function createTextarea() {
-  return `<section class="text">
-  <textarea class="text__enter" name="input box" id="text" cols="100" rows="10"></textarea>
-</section>`
-}
-
-//main.innerHTML = createTextarea();
+// Создаем элементы клавиатуры
+const text = document.createElement("section");
+text.classList.add('text');
+const textEnter = document.createElement("textarea");
+textEnter.classList.add('text__enter');
+text.appendChild(textEnter)
+main.appendChild(text);
