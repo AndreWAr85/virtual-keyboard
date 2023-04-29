7600304c28f920430ae5a728e591090018486aa9
AndreWAr85 <andreyarabchyk@gmail.com>
2023-04-29 09:10:44 +0200

    feat: add tempori keys

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -3,21 +3,7 @@ const main = document.querySelector('main');
 function createTextarea() {
   return `<section class="text">
   <textarea class="text__enter" name="input box" id="text" cols="100" rows="10"></textarea>
-</section>
-<section class="keyboard">
-  <div class="keyboard__container">
-    <button class="keyboard__key" type="button">a</button>
-    <button class="keyboard__key key-max" type="button">
-      <span class="material-icons">backspace</span>
-    </button>
-    <button class="keyboard__key key-supermax" type="button">
-      <span class="material-icons">space_bar</span>
-    </button>
-    <button class="keyboard__key key-max key-active" type="button">
-      <span class="keyboard-name">Caps Lock</span>
-    </button>
-  </div>
-</section>`;
+</section>`
 }
 
-main.innerHTML = createTextarea();
+//main.innerHTML = createTextarea();
