e0035d3b9c9631e2672e7ac952b588b4b588aefb
AndreWAr85 <andreyarabchyk@gmail.com>
2023-05-01 22:15:58 +0200

    feat: Add modular structure for project

---
+++ src/assets/js/style.js
@@ -0,0 +1,75 @@
+export const keyStyleMap = {
+  Backspace: {
+    backgroundColor: '#90e2c4',
+    width: '100px',
+    fontSize: '18px',
+  },
+  Tab: {
+    backgroundColor: '#90e2c4',
+    fontSize: '18px',
+  },
+  Delete: {
+    backgroundColor: '#90e2c4',
+    fontSize: '14px',
+  },
+  CapsLock: {
+    // backgroundColor: '#90e2c4',
+    width: '96px',
+    fontSize: '18px',
+    classList: ['capslock'],
+  },
+  Enter: {
+    backgroundColor: '#90e2c4',
+    width: '100px',
+    fontSize: '18px',
+  },
+  '↑': {
+    backgroundColor: '#90e2c4',
+  },
+  Control: {
+    backgroundColor: '#90e2c4',
+    width: '50px',
+    fontSize: '12px',
+    classList: ['control'],
+  },
+  Win: {
+    backgroundColor: '#90e2c4',
+    width: '50px',
+    fontSize: '18px',
+    classList: ['meta'],
+  },
+  ' ': {
+    width: '318px',
+  },
+  Alt: {
+    backgroundColor: '#90e2c4',
+    width: '50px',
+    fontSize: '18px',
+    classList: ['alt'],
+  },
+  '←': {
+    backgroundColor: '#90e2c4',
+  },
+  '↓': {
+    backgroundColor: '#90e2c4',
+  },
+  '→': {
+    backgroundColor: '#90e2c4',
+    width: '64px',
+  },
+  '`': {
+    backgroundColor: '#90e2c4',
+  },
+  ё: {
+    backgroundColor: '#90e2c4',
+  },
+  '\\': {
+    backgroundColor: '#90e2c4',
+  },
+  Shift: {
+    backgroundColor: '#90e2c4',
+    width: '65px',
+    fontSize: '18px',
+    classList: ['shift'],
+  },
+};
